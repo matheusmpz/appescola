dd19b8c9b110f812c26383010cb72b43cf3761e9
Matheus Pereira <matheus.p.soares242@gmail.com>
2025-02-07 16:33:12 -0300

    chore: update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -7,8 +7,101 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Bem vindo,'),
-
+        toolbarHeight: 100,
+        title: const Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Bem vindo,",
+              style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Archivo',
+                  color: Color(0xFF061F3E)),
+            ),
+            Text(
+              "Samuel Kanudo!",
+              style: TextStyle(
+                  fontSize: 24,
+                  fontFamily: 'Archivo',
+                  color: Color(0xFF061F3E)),
+            ),
+          ],
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              // Ação do botão
+            },
+            icon: const Icon(
+              Icons.notifications,
+              color: Color(0xFF061F3E),
+              size: 32,
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            // Container com a imagem de fundo
+            Container(
+              width: double.infinity, // Ocupa toda a largura
+              height: 350, // Altura ajustável
+              padding: const EdgeInsets.all(16), // Espaço interno
+              decoration: BoxDecoration(
+                image: const DecorationImage(
+                  image: AssetImage(
+                      'assets/imagem_fundo.jpg'), // Caminho da imagem
+                  fit: BoxFit.cover, // Ajusta a imagem no container
+                ),
+                borderRadius: BorderRadius.circular(16), // Borda arredondada
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Acesse sua",
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Color(0xFF061F3E), // Cor do texto
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  const Text(
+                    "Agenda Virtual",
+                    style: TextStyle(
+                      fontSize: 24,
+                      color: Color(0xFF061F3E),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  ElevatedButton(
+                    onPressed: () {
+                      // Ação do botão
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue, // Cor do botão
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24, vertical: 12), // Espaçamento interno
+                      shape: RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.circular(8), // Borda arredondada
+                      ),
+                    ),
+                    child: const Text(
+                      "Ver mais",
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
